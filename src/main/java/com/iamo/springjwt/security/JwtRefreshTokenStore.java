979b5980fb6e40bd9adcdaca5080fb737dd75d00
11424280 <pongchai.boonmee@krungsri.com>
2025-06-12 11:53:39 +0700

    add redis

--- src/main/java/com/iamo/springjwt/security/JwtRefreshTokenStore.java
+++ src/main/java/com/iamo/springjwt/security/JwtRefreshTokenStore.java
@@ -1,24 +1,36 @@
 package com.iamo.springjwt.security;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.time.Duration;
 
 @Component
 public class JwtRefreshTokenStore {
-    private final Map<String, String> refreshTokens = new ConcurrentHashMap<>();
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public JwtRefreshTokenStore(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
 
     public void save(String username, String refreshToken) {
-        refreshTokens.put(username, refreshToken);
+        redisTemplate.opsForValue().set(username, refreshToken, Duration.ofSeconds(6000));
     }
 
     public boolean isValid(String username, String refreshToken) {
-        return refreshToken.equals(refreshTokens.get(username));
+
+        Object cachedUser = redisTemplate.opsForValue().get(username);
+
+        if (cachedUser != null) {
+            return cachedUser.equals(refreshToken);
+        }
+        return false;
     }
 
     public void remove(String username) {
-        refreshTokens.remove(username);
+        redisTemplate.delete(username);
     }
 }
 
